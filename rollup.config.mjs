81bba2875380a9bac7fd00a03fc292cdae3c04a1
Artak Harutyunyan <artak_harutyunyan@mail.com>
2026-03-05 19:38:21 +0400

    RSA-8: Rollup config update

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,17 +5,28 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
   plugins: [
     peerDepsExternal(),
-    resolve(),
+    resolve({ extensions: ['.js', '.jsx'] }),
     commonjs(),
     babel({
       babelHelpers: 'bundled',
-      presets: ['@babel/preset-react'],
+      extensions: ['.js', '.jsx'],
+      presets: [
+        ['@babel/preset-env', { targets: '> 0.25%, not dead', modules: false }],
+        ['@babel/preset-react', { runtime: 'automatic' }],
+      ],
     }),
   ],
+  external: ['react', 'react-dom'],
 };
